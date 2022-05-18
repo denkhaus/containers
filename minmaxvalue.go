faf69b5f0c1eb1d45f27f2b5a779b31a91d8bf9e
denkhaus <peristaltic@gmx.net>
2022-05-18 09:20:32 +0200

    modified update, add ReadFrom and WriteTo

--- minmaxvalue.go
+++ minmaxvalue.go
@@ -4,11 +4,13 @@ import (
 	"bytes"
 	"encoding/gob"
 	"fmt"
+	"io"
 
 	"golang.org/x/exp/constraints"
 )
 
 type MinMaxValue[T constraints.Ordered] struct {
+	def      T
 	minValue T
 	curValue T
 	maxValue T
@@ -22,7 +24,23 @@ func NewMinMaxValue[T constraints.Ordered](v T) *MinMaxValue[T] {
 	}
 }
 
+func (p *MinMaxValue[T]) Reset() {
+	p.minValue = p.def
+	p.curValue = p.def
+	p.maxValue = p.def
+}
+
+func (p *MinMaxValue[T]) isUntouched() bool {
+	return p.minValue == p.def && p.curValue == p.def && p.maxValue == p.def
+}
+
 func (p *MinMaxValue[T]) Update(v T) {
+	if p.isUntouched() {
+		p.minValue = v
+		p.curValue = v
+		p.maxValue = v
+	}
+
 	p.minValue = Min(p.minValue, v)
 	p.curValue = v
 	p.maxValue = Max(p.maxValue, v)
@@ -44,26 +62,32 @@ func (p *MinMaxValue[T]) Format(format string, a ...any) string {
 	return fmt.Sprintf(format, append([]any{p.Min(), p.Cur(), p.Max()}, a...)...)
 }
 
-func (p *MinMaxValue[T]) GobEncode() ([]byte, error) {
-	buffer := bytes.NewBuffer(nil)
-	enc := gob.NewEncoder(buffer)
-
+func (p *MinMaxValue[T]) WriteTo(w io.Writer) error {
+	enc := gob.NewEncoder(w)
 	if err := enc.Encode(p.minValue); err != nil {
-		return nil, err
+		return err
 	}
 	if err := enc.Encode(p.curValue); err != nil {
-		return nil, err
+		return err
 	}
 	if err := enc.Encode(p.maxValue); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (p *MinMaxValue[T]) GobEncode() ([]byte, error) {
+	buffer := bytes.NewBuffer(nil)
+	if err := p.WriteTo(buffer); err != nil {
 		return nil, err
 	}
 
 	return buffer.Bytes(), nil
 }
 
-func (p *MinMaxValue[T]) GobDecode(v []byte) error {
-	dec := gob.NewDecoder(bytes.NewBuffer(v))
-
+func (p *MinMaxValue[T]) ReadFrom(r io.Reader) error {
+	dec := gob.NewDecoder(r)
 	if err := dec.Decode(&p.minValue); err != nil {
 		return err
 	}
@@ -75,4 +99,8 @@ func (p *MinMaxValue[T]) GobDecode(v []byte) error {
 	}
 
 	return nil
+}
+
+func (p *MinMaxValue[T]) GobDecode(v []byte) error {
+	return p.ReadFrom(bytes.NewBuffer(v))
 }
