87831d184fb24039d8512824bf8070fa83b55b6c
Brain Loop <peristaltic@gmx.net>
2023-01-25 18:00:49 +0100

    Tuple make properties public

--- tuple.go
+++ tuple.go
@@ -1,19 +1,19 @@
 package containers
 
 type Tuple[T1 any, T2 any] struct {
-	v1 T1
-	v2 T2
+	T1Val T1
+	T2Val T2
 }
 
 func (p *Tuple[T1, T2]) Set(v1 T1, v2 T2) {
-	p.v1 = v1
-	p.v2 = v2
+	p.T1Val = v1
+	p.T2Val = v2
 }
 
 func (p *Tuple[T1, T2]) Get1() T1 {
-	return p.v1
+	return p.T1Val
 }
 
 func (p *Tuple[T1, T2]) Get2() T2 {
-	return p.v2
+	return p.T2Val
 }
