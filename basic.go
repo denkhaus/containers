dc59a550f919511a7a4a8aec2bcc4006989f8913
denkhaus <peristaltic@gmx.net>
2025-05-18 19:08:50 +0200

    add documentation for Max, Min, MaxValue, MinValue, BetweenInclusive, and BetweenExclusive functions

--- basic.go
+++ basic.go
@@ -28,6 +28,12 @@ func Mult[T Number](a, b T) T {
 	return a * b
 }
 
+// Max returns the larger of a and b.
+// If a == b, a is returned.
+//
+// This function is intended to be used with types that support
+// the constraints.Ordered type constraint, such as int, float64,
+// and string.
 func Max[T constraints.Ordered](a, b T) T {
 	if a > b {
 		return a
@@ -39,6 +45,11 @@ func Max[T constraints.Ordered](a, b T) T {
 	return a
 }
 
+// Min returns the smaller of a and b.
+// If a == b, a is returned.
+// This function is intended to be used with types that support
+// the constraints.Ordered type constraint, such as int, float64,
+// and string.
 func Min[T constraints.Ordered](a, b T) T {
 	if a < b {
 		return a
@@ -50,6 +61,8 @@ func Min[T constraints.Ordered](a, b T) T {
 	return a
 }
 
+// MaxValue returns the largest value in a slice.
+// Assumes the slice is non-empty.
 func MaxValue[T constraints.Ordered](a []T) T {
 	m := a[0]
 	for _, v := range a {
@@ -60,6 +73,8 @@ func MaxValue[T constraints.Ordered](a []T) T {
 	return m
 }
 
+// MinValue returns the smallest value in a slice.
+// If the slice is empty, it returns the zero value of type T.
 func MinValue[T constraints.Ordered](a []T) T {
 	m := a[0]
 	for _, v := range a {
@@ -68,4 +83,14 @@ func MinValue[T constraints.Ordered](a []T) T {
 		}
 	}
 	return m
+}
+
+// Between checks if val is between min and max, inclusive.
+func BetweenInclusive[T constraints.Ordered](min, max, val T) bool {
+	return min <= val && val <= max
+}
+
+// Between checks if val is between min and max, exclusive.
+func BetweenExclusive[T constraints.Ordered](min, max, val T) bool {
+	return min < val && val < max
 }
