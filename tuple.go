83b09f039fe873bf64f8bb33596cf9c7201d4ae5
denkhaus <peristaltic@gmx.net>
2025-06-05 12:11:07 +0200

    refactor: improve documentation for Tuple methods

--- tuple.go
+++ tuple.go
@@ -5,15 +5,33 @@ type Tuple[T1 any, T2 any] struct {
 	T2Val T2
 }
 
+// Set sets the value of the tuple.
+//
+// This is a convenience function for updating the entire tuple at once.
 func (p *Tuple[T1, T2]) Set(v1 T1, v2 T2) {
 	p.T1Val = v1
 	p.T2Val = v2
 }
 
+// Get1 returns the value of the first element of the tuple.
+
 func (p *Tuple[T1, T2]) Get1() T1 {
 	return p.T1Val
 }
 
+// Get2 returns the value of the second element of the tuple.
+
 func (p *Tuple[T1, T2]) Get2() T2 {
 	return p.T2Val
+}
+
+// NewTupple creates a new instance of Tuple with the given values.
+//
+// Example:
+//
+//	t := NewTupple(1, "hello")
+//	t.Get1() // returns 1
+//	t.Get2() // returns "hello"
+func NewTupple[T1 any, T2 any](v1 T1, v2 T2) *Tuple[T1, T2] {
+	return &Tuple[T1, T2]{T1Val: v1, T2Val: v2}
 }
